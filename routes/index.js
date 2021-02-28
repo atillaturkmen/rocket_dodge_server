4459a432bb165e9435a8b23098d058a45c866ea8
atillaturkmen <turkmenatilla522@gmail.com>
2021-02-28 19:37:46 +0300

    folderization

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,7 @@ router.get("/", function (req, res) {
 });
 
 router.get("/favicon.ico", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../favicon.ico"));
+    res.sendFile(path.join(__dirname, "/../public/favicon.ico"));
 });
 
 router.get("/about", function (req, res) {
